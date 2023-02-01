4f1c2ce6da2261cd80d1fa925a8b714d31e139d5
MohamedKhedr <112129513+MohamedKhedr46@users.noreply.github.com>
2023-02-01 03:42:53 +0200

    Add files via upload

---
+++ Autonomous Car/Autonomous Car/APP/main.c
@@ -0,0 +1,98 @@
+/*
+* Autonomous Car.c
+*
+* Created: 1/29/2023 4:38:16 AM
+* Author : MohamedKhedr
+*/
+
+#include "..\\HAL\\Servo.h"
+#include "..\\HAL\\Ultrasonic.h"
+#include "..\\HAL\\Motion.h"
+#include "..\\HAL\\Lcd.h"
+
+
+int main(void)
+{
+	
+	motion_init();
+	servo_init();
+	LCD_Init();
+	ultrasonic_init();
+	
+
+	_delay_ms(1000);
+	
+	while(1)
+	{
+		
+		
+		ultrasonic_triger();
+		_delay_ms(15);
+		
+		if (distance>15)
+		{			
+			forward();
+			LCD_Forward();
+			
+		}
+
+		if(distance < 15)
+		{
+			stop();
+			LCD_STOPPED();
+			
+			Servo_Left();
+			_delay_ms(4500);
+			
+			ultrasonic_triger();
+			_delay_ms(15);
+			
+			if(distance > 15)
+			{
+				Servo_Right();
+				_delay_ms(4500);
+				
+				Servo_Front();
+				
+				left();
+				LCD_Left();
+				_delay_ms(3700);
+				
+			}
+			
+			else if(distance < 15)
+			{
+				Servo_Right();
+				_delay_ms(4500);
+				
+				ultrasonic_triger();
+				_delay_ms(15);
+				
+				if (distance < 15)
+				{
+					Servo_Front();
+					Xturn();
+					LCD_Xturn();
+					_delay_ms(7200);
+					
+				}
+				
+				else if (distance > 15)
+				{
+					Servo_Front();
+					right();
+					LCD_Right();
+					_delay_ms(3700);
+					
+				}
+			}
+
+
+		}
+		
+
+	}
+
+
+}
+
